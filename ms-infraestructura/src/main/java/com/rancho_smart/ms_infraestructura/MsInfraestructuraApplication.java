1b6904b2427341b5d07e64149e1b3be484ba455a
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 03:10:48 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-infraestructura/src/main/java/com/rancho_smart/ms_infraestructura/MsInfraestructuraApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_infraestructura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInfraestructuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInfraestructuraApplication.class, args);
+	}
+
+}
---
+++ ms-infraestructura/src/test/java/com/rancho_smart/ms_infraestructura/MsInfraestructuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_infraestructura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInfraestructuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
